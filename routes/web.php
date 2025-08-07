c78e7ae67de0c3ea95801af724790097eadb621c
khadijabayazid <khokhaba2005@gmail.com>
2025-08-07 23:12:02 +0300

    New Design Layout for Blog Project

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::view('/second', 'second');
 Route::view('/', 'home')->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
+Route::view('article','article')->name('article');
 
